448b98a287f2d22d04dad21f90be3d38ca5696b9
sunilhari <sunilhari22@hotmail.com>
2015-01-16 23:04:51 +0530

    updated server for advanced key bindings

--- Controller/src/com/sun/constants/CONSTANTS.java
+++ Controller/src/com/sun/constants/CONSTANTS.java
@@ -1,9 +1,9 @@
 package com.sun.constants;
 
-import java.net.InetAddress;
 
 public class CONSTANTS {
 	public static int PORT_NUMBER = 8090;
 	public static String SERVER_IP = "10.1.3.11";
-	
+	public static int autoDelayRobot = 0;
+	public static int keyConfig = 1;
 }
--- Controller/src/com/sun/utils/Emulator.java
+++ Controller/src/com/sun/utils/Emulator.java
@@ -2,16 +2,16 @@ package com.sun.utils;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.awt.event.KeyEvent;
 
 public class Emulator {
 	private Robot emulate = null;
-	private int previousKeyCode = 0;
-	
+	private KeyMap mapper;
 
 	public Emulator() {
 		try {
 			emulate = new Robot();
+			mapper = new KeyMap();//argument indicates the type of key to be used
+			
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,70 +20,45 @@ public class Emulator {
 
 	public void emulateKeypress(String rkey) {
 		int code = Integer.parseInt(rkey.trim());
-		emulate.keyRelease(KeyEvent.VK_W);
-		emulate.keyRelease(KeyEvent.VK_A);
-		emulate.keyRelease(KeyEvent.VK_S);
-		emulate.keyRelease(KeyEvent.VK_D);
+		emulate.keyRelease(mapper.UP);
+		emulate.keyRelease(mapper.LEFT);
+		emulate.keyRelease(mapper.DOWN);
+		emulate.keyRelease(mapper.RIGHT);
 		switch (code) {
-		case 1://left
-			emulate.keyPress(KeyEvent.VK_A);
+		case 1:// left
+			emulate.keyPress(mapper.LEFT);
 			break;
-		case 2://right
-			emulate.keyPress(KeyEvent.VK_D);
+		case 2:// right
+			emulate.keyPress(mapper.RIGHT);
 			break;
-		case 4://down
-			emulate.keyPress(KeyEvent.VK_S);
+		case 4:// down
+			emulate.keyPress(mapper.DOWN);
 			break;
-		case 8://up
-			emulate.keyPress(KeyEvent.VK_W);
+		case 8:// up
+			emulate.keyPress(mapper.UP);
 			break;
-		case 6://down right
-			emulate.keyPress(KeyEvent.VK_S);
-			emulate.keyPress(KeyEvent.VK_D);
+		case 6:// down right
+			emulate.keyPress(mapper.DOWN);
+			emulate.keyPress(mapper.RIGHT);
 			break;
-		case 9://up left
-			emulate.keyPress(KeyEvent.VK_W);
-			emulate.keyPress(KeyEvent.VK_A);
+		case 9:// up left
+			emulate.keyPress(mapper.UP);
+			emulate.keyPress(mapper.LEFT);
 			break;
-		case 5://down left
-			emulate.keyPress(KeyEvent.VK_S);
-			emulate.keyPress(KeyEvent.VK_A);
+		case 5:// down left
+			emulate.keyPress(mapper.DOWN);
+			emulate.keyPress(mapper.LEFT);
 			break;
-		case 10://up right
-			emulate.keyPress(KeyEvent.VK_W);
-			emulate.keyPress(KeyEvent.VK_D);
+		case 10:// up right
+			emulate.keyPress(mapper.UP);
+			emulate.keyPress(mapper.RIGHT);
 			break;
 		default:
-			emulate.keyRelease(KeyEvent.VK_W);
-			emulate.keyRelease(KeyEvent.VK_A);
-			emulate.keyRelease(KeyEvent.VK_S);
-			emulate.keyRelease(KeyEvent.VK_D);
+			emulate.keyRelease(mapper.UP);
+			emulate.keyRelease(mapper.LEFT);
+			emulate.keyRelease(mapper.DOWN);
+			emulate.keyRelease(mapper.RIGHT);
 			break;
 		}
 	}
-
-	/*public int getKeyCode(String key) {
-		int event = 0;
-		switch (key) {
-		case "UP":
-			event = KeyEvent.VK_W;
-			
-			break;
-		case "DOWN":
-			event = KeyEvent.VK_S;
-			break;
-		case "LEFT":
-			event = KeyEvent.VK_A;
-			
-			break;
-		case "RIGHT":
-			event = KeyEvent.VK_D;
-			
-			break;
-		default:
-			break;
-		}
-		
-		return event;
-	}*/
 }

43ab4c9489157a903efc85ad97137476de96e2e9
sunilhari <sunilhari22@hotmail.com>
2015-01-16 09:16:49 +0530

    updated server for improved performance

--- Controller/src/com/sun/constants/CONSTANTS.java
+++ Controller/src/com/sun/constants/CONSTANTS.java
@@ -5,4 +5,5 @@ import java.net.InetAddress;
 public class CONSTANTS {
 	public static int PORT_NUMBER = 8090;
 	public static String SERVER_IP = "10.1.3.11";
+	
 }
--- Controller/src/com/sun/utils/Emulator.java
+++ Controller/src/com/sun/utils/Emulator.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 
 public class Emulator {
 	private Robot emulate = null;
-
+	private int previousKeyCode = 0;
 	
 
 	public Emulator() {
@@ -19,31 +19,65 @@ public class Emulator {
 	}
 
 	public void emulateKeypress(String rkey) {
-		try{
-		System.out.println("Emulate " + rkey);
-		emulate.keyPress(getKeyCode(rkey));
-		}catch(Exception e){
-			e.printStackTrace();
+		int code = Integer.parseInt(rkey.trim());
+		emulate.keyRelease(KeyEvent.VK_W);
+		emulate.keyRelease(KeyEvent.VK_A);
+		emulate.keyRelease(KeyEvent.VK_S);
+		emulate.keyRelease(KeyEvent.VK_D);
+		switch (code) {
+		case 1://left
+			emulate.keyPress(KeyEvent.VK_A);
+			break;
+		case 2://right
+			emulate.keyPress(KeyEvent.VK_D);
+			break;
+		case 4://down
+			emulate.keyPress(KeyEvent.VK_S);
+			break;
+		case 8://up
+			emulate.keyPress(KeyEvent.VK_W);
+			break;
+		case 6://down right
+			emulate.keyPress(KeyEvent.VK_S);
+			emulate.keyPress(KeyEvent.VK_D);
+			break;
+		case 9://up left
+			emulate.keyPress(KeyEvent.VK_W);
+			emulate.keyPress(KeyEvent.VK_A);
+			break;
+		case 5://down left
+			emulate.keyPress(KeyEvent.VK_S);
+			emulate.keyPress(KeyEvent.VK_A);
+			break;
+		case 10://up right
+			emulate.keyPress(KeyEvent.VK_W);
+			emulate.keyPress(KeyEvent.VK_D);
+			break;
+		default:
+			emulate.keyRelease(KeyEvent.VK_W);
+			emulate.keyRelease(KeyEvent.VK_A);
+			emulate.keyRelease(KeyEvent.VK_S);
+			emulate.keyRelease(KeyEvent.VK_D);
+			break;
 		}
 	}
 
-	public int getKeyCode(String key) {
+	/*public int getKeyCode(String key) {
 		int event = 0;
 		switch (key) {
 		case "UP":
-			event = KeyEvent.VK_UP;
+			event = KeyEvent.VK_W;
 			
 			break;
 		case "DOWN":
-			event = KeyEvent.VK_DOWN;
-			
+			event = KeyEvent.VK_S;
 			break;
 		case "LEFT":
-			event = KeyEvent.VK_LEFT;
+			event = KeyEvent.VK_A;
 			
 			break;
 		case "RIGHT":
-			event = KeyEvent.VK_RIGHT;
+			event = KeyEvent.VK_D;
 			
 			break;
 		default:
@@ -51,5 +85,5 @@ public class Emulator {
 		}
 		
 		return event;
-	}
+	}*/
 }
